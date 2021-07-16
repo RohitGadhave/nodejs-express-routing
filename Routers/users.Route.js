2459f3bc07756551b9a97aef83dce2f616bacc2d
Rohit Gadhave <RohitGadhave249@gmail.com>
2021-07-16 19:53:07 +0530

    Route parameters

--- Routers/users.Route.js
+++ Routers/users.Route.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 });
 
 // GET method route
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
     res.status(200).send("hello i am get with user router");
 });
 // POST method route
@@ -39,5 +39,51 @@ router.delete('/:id', (req, res) => {
 router.get('/:userId/books/:bookId',(req,res)=>{
     res.json(req.params);
 });
+
+
+
+/*
+Route path: /user/:from-:to
+Request URL: http://localhost:3000/users/100-300
+req.params: { "from": "100", "to": "300" }
+*/
+
+router.get('/:from-:to',(req,res)=>{
+    res.json(req.params);
+});
+
+/*
+Route path: /user/:from.:to
+Request URL: http://localhost:3000/users/100-300
+req.params: { "from": "100", "to": "300" }
+*/
+
+router.get('/:from.:to1',(req,res)=>{
+    res.json(req.params);
+});
+
+/*
+Route path: /user/:userId(\d+)
+Request URL: http://localhost:3000/user/42
+req.params: {"userId": "42"}
+*/
+
+router.get('/user/:userId(\\d+)',(req,res)=>{
+    res.json(req.params);
+});
+
+/*
+method:POST
+Route path: /jsonobj
+Content-Type: application/json
+Request URL: http://localhost:3000/user/json
+req.body: {"userId": "42","jxbjb":"z"}
+*/
+
+router.post('/jsonobj',(req,res)=>{
+    //console.log(req.body);
+    res.json(req.body);
+});
+
 
 module.exports = router;
--- main.js
+++ main.js
@@ -7,6 +7,9 @@ const usersRouter = require('./Routers/users.Route');
 const app = express();
 dotenv.config();
 
+//accept request data in json format/type 
+app.use(express.json());//for json type
+
 app.get('/',(req,res)=>{
 res.status(200).send(`use
 GET http://localhost:${process.env.PORT || 3000}/user
